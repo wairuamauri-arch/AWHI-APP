e92abb43f4f6d9ec4de08b27a7c56c899deb7413
wairuamauri-arch <wairua.mauri@gmail.com>
2026-08-28 09:35:55 +1200

    Add basic PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = 'awhi-shell-v1';
+const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
