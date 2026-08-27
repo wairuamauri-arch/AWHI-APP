d399f0e515a3f8330c5cba3d685b5d35ec77734a
wairuamauri-arch <wairua.mauri@gmail.com>
2026-08-28 10:37:05 +1200

    Refresh PWA cache for authentication build

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,16 @@
-const CACHE = 'awhi-shell-v1';
+const CACHE = 'awhi-shell-v2';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
